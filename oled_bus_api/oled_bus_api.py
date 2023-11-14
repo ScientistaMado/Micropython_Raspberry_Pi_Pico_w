59b0b90ec3a0faee32a15e769ed2e72271c76128
ScientistaMado <profesorsamuelcortes@gmail.com>
2023-11-14 11:56:08 -0300

    Oled bus api v1.0

--- oled_bus_api/oled_bus_api.py
+++ oled_bus_api/oled_bus_api.py
@@ -3,6 +3,7 @@ import gc                      # Para recolección de basura
 import time                    # Para manejar el tiempo
 import urequests as requests   # Para realizar solicitudes HTTP
 import ssd1306                 # Display oled
+import framebuf                # Para trabajar con imágenes
 import json                    # Manipular archivos Json
 from machine import Pin, I2C
 
@@ -24,11 +25,24 @@ def connectWifi(ssid, password):
     station.active(True)
     station.connect(ssid, password)
 
+    oled.fill(0)
+    oled.show()
+    oled.text("Conectando", 10, 10)
+    oled.show()
+    dot = 10
+
     while not station.isconnected():
-        print('Conectando...')
+        print('conectando')
+        oled.text(".", dot, 20)
+        oled.show()
         time.sleep(1)
+        dot += 6
 
     print(f'Conexión exitosa a {ssid}')
+
+    oled.fill(0)
+    oled.text("Conectado", 10, 10)
+    oled.show()
 
 
 def fetchApiBus(api_url, stop_bus, timeout=10):
@@ -50,6 +64,18 @@ def fetchApiBus(api_url, stop_bus, timeout=10):
         return None
 
 
+def openIcon(icon_id):
+
+    with open(f'icons/{icon_id}.pbm', "rb") as file:
+        file.readline()
+        xy = file.readline()
+        x = int(xy.split()[0])
+        y = int(xy.split()[1])
+        icon = bytearray(file.read())
+
+    return framebuf.FrameBuffer(icon, x, y, framebuf.MONO_HLSB)
+
+
 def showInOled(stop_bus_info, bus_id):
     """
     Muestra en el display oled la información del bus
@@ -67,12 +93,19 @@ def showInOled(stop_bus_info, bus_id):
             min_time = service['buses'][0]['min_arrival_time']
             max_time = service['buses'][0]['max_arrival_time']
 
+    def oledCenterText(text, y):
+        x = int((128 - len(text)*8)/2)
+        oled.text(text, x, y)
+
+    time_interval = f'{min_time} - {max_time} min'
+    d = f'{distance} m'
+
     oled.fill(0)
-    oled.text(id_stop_bus, 0, 0)
-    oled.text(str(bus), 0, 8)
-    oled.text(str(distance), 0, 16)
-    oled.text(str(min_time), 0, 24)
-    oled.text(str(max_time), 0, 32)
+    oled.blit('icon_bus', 0, 10)
+    oled.text(id_stop_bus, 27, 0)
+    oled.text(bus_id, 27, 8)
+    oledCenterText(d, 16)
+    oledCenterText(time_interval, 24)
     oled.show()
 
 
