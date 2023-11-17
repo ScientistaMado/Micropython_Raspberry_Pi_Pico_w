02643fc6036063543016686dc4453454258037e0
ScientistaMado <profesorsamuelcortes@gmail.com>
2023-11-17 17:46:00 -0300

    Se incluyeron comentarios adicionales

--- BLE_examples/BLE_send_receive.py
+++ BLE_examples/BLE_send_receive.py
@@ -5,16 +5,21 @@ import bluetooth
 from ble_simple_peripheral import BLESimplePeripheral
 
 
+# Create object BLE
 ble = bluetooth.BLE()
 
+# Create object Simple peripheral and active BLE
 sp = BLESimplePeripheral(ble)
 
+# Configure the LED pin as output and the button pin as pulldown
 led = Pin(15, Pin.OUT)
 button = Pin(14, Pin.IN, Pin.PULL_DOWN)
-led_state = 0
 
 
 def on_rx(data):
+    """"
+    function that is executed when the on_off data is received\r\n
+    """
     print("Data received: ", data)
     if data == b'on_off\r\n':
         led.value(not led.value())
@@ -25,11 +30,15 @@ def on_rx(data):
 
 
 while True:
+
     if sp.is_connected():
+     # If there is data received
 
-        sp.on_write(on_rx)
+        sp.on_write(on_rx)  # Execute the on_rx function
 
-        if button.value():
-            sp.send("button pressed\n")
-            print("Boton presionado")
-            time.sleep(0.1)
+    if button.value():
+        # If button is pressed, sends button pressed
+
+        sp.send("button pressed\n")
+        print("Boton presionado")
+        time.sleep(0.1)
