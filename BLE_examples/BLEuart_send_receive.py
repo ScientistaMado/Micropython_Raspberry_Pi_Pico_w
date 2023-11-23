926607c4424b0872e7724a555d68cf7d35b2c829
ScientistaMado <profesorsamuelcortes@gmail.com>
2023-11-23 09:50:00 -0300

    Se ha agregado biblioteca BLEuart

---
+++ BLE_examples/BLEuart_send_receive.py
@@ -0,0 +1,21 @@
+from machine import Pin
+import bluetooth
+from ble_uart_peripheral import BLEUART
+
+ble = bluetooth.BLE()
+ble_uart = BLEUART(ble)
+
+button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+led = Pin(15, Pin.OUT)
+
+while True:
+    if button.value():
+        ble_uart.write("Button pressed\r\n")
+
+    if ble_uart.any():
+        data = ble_uart.read()
+
+        if data == b'on_off\r\n':
+            led.value(not led.value())
+        else:
+            ble_uart.write('Command is not recognized\r\n"')
