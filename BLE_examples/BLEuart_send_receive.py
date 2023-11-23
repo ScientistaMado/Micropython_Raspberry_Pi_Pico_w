64138e35a0d100d2f696a0dbd9d0b76470740982
ScientistaMado <profesorsamuelcortes@gmail.com>
2023-11-23 09:52:38 -0300

    Se ha agregado ejemplo BLEuart

--- BLE_examples/BLEuart_send_receive.py
+++ BLE_examples/BLEuart_send_receive.py
@@ -12,8 +12,9 @@ while True:
     if button.value():
         ble_uart.write("Button pressed\r\n")
 
+    # queries if there is received data stored in the buffer
     if ble_uart.any():
-        data = ble_uart.read()
+        data = ble_uart.read()  # reads the data removing it from the buffer
 
         if data == b'on_off\r\n':
             led.value(not led.value())
