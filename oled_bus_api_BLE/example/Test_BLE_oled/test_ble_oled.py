6f42c9d69e9f2652787b35e5c501041bbf59ffef
ScientistaMado <profesorsamuelcortes@gmail.com>
2023-11-24 11:50:51 -0300

    Se actualizaron comentarios

--- oled_bus_api_BLE/example/Test_BLE_oled/test_ble_oled.py
+++ oled_bus_api_BLE/example/Test_BLE_oled/test_ble_oled.py
@@ -1,8 +1,8 @@
-import ssd1306                 # Display oled
-import json                    # Manipular archivos Json
-from machine import Pin, I2C   # Control pines y puerto I2C
-import bluetooth
-from ble_uart_peripheral import BLEUART
+import ssd1306                            # Display oled
+import json                               # Manipular archivos Json
+from machine import Pin, I2C              # Control pines y puerto I2C
+import bluetooth                          # Control general BLE
+from ble_uart_peripheral import BLEUART   # Funciones específicas BLE uart
 
 ble = bluetooth.BLE()
 ble_uart = BLEUART(ble)
@@ -14,15 +14,18 @@ i2c = I2C(0, scl=Pin(17), sda=Pin(16))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c, 0x3c)
 
 while True:
+    # Consulta si el pulsador está presionado
     if button.value():
+
         oled.fill(0)
         oled.text("Button pressed", 0, 0)
         oled.show()
         ble_uart.write("Button pressed\r\n")
 
-    # queries if there is received data stored in the buffer
+    # Consulta si hay datos disponibles para leer en el buffer
     if ble_uart.any():
-        data = ble_uart.read()  # reads the data removing it from the buffer
+
+        data = ble_uart.read()  # Leemos los datos, quitándolos del buffer
 
         if data == b'on_off\r\n':
             led.value(not led.value())
