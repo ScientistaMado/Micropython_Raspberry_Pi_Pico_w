607c9024f8bc9c2400096665f0bddfa9795e2c39
ScientistaMado <profesorsamuelcortes@gmail.com>
2023-11-27 10:06:29 -0300

    Se actualizó ruta de plantillas

---
+++ Templates/api_requests/README.md
@@ -0,0 +1,20 @@
+# Programa base micropython Raspberry Pi Pico W consulta API
+
+![ESP8266](https://img.shields.io/badge/ESP8266-Compatible-blue)
+
+![Micropython_Raspberry_Pi_Pico_w](https://img.shields.io/badge/Raspberry%20Pi%20Pico%20W-Micropython%20Compatible-brightgreen)
+
+## Instrucciones
+
+1. Reemplaza SSID, PASSWORD y URL_API con los datos de que necesites. Por el momento el código
+   sirve para solicitudes http. Para solicitudes https puede generar errores.
+2. Para realizar pruebas puedes obtener una URL_API registrándote en https://www.meteored.cl/api/
+   Ahí encontrarás datos meteorológicos en formato XML y Json para tu localidad
+
+## Requisitos
+
+- Raspberry Pi Pico W
+- Acceso a internet
+- URL de la API a la que se desea acceder
+
+¡Disfruta de tu proyecto de conexión a una red WiFi y acceso a una API con NodeMCU ESP8266!
---
+++ Templates/api_requests/api_requests.py
@@ -0,0 +1,65 @@
+""" 
+Este código es la base para conectar una Raspberry Pi Pico W a una red WiFi y realizar una solicitud a una API.
+
+1. Se importan las bibliotecas necesarias y se realiza una recolección de basura para liberar memoria.
+2. Se configuran las credenciales de la red WiFi y la URL de la API a la que se va a acceder.
+3. Se define una función para conectarse a la red WiFi y otra para obtener datos de la API.
+4. Después de conectarse con éxito a la red WiFi, se realiza una solicitud a la API y se muestra la respuesta.
+
+"""
+
+
+import network  # Para gestionar la conexión a la red
+import gc       # Para recolección de basura
+import time     # Para manejar el tiempo
+import urequests as requests  # Para realizar solicitudes HTTP
+
+# Limpiar la memoria
+gc.collect()
+
+SSID = 'TU SSID'        # Nombre de la red WiFi
+PASSWORD = 'TU PASS'    # Contraseña de la red WiFi
+URL_API = 'TU URL API'  # URL de la API a la que se va a acceder
+
+# Función para conectar a la red WiFi
+
+
+def connectWifi(ssid, password):
+    # Inicializar la interfaz de estación
+
+    station = network.WLAN(network.STA_IF)
+    station.active(True)                    # Activar la interfaz
+    station.connect(ssid, password)
+
+    while not station.isconnected():
+        print('Conectando...')
+        time.sleep(1)
+
+    print(f'Conexión exitosa a {ssid}')
+
+
+def fetchApi(api_url, timeout=10):
+    # Función para obtener datos de la API
+
+    try:
+        # Realizar una solicitud GET
+        response = requests.get(api_url, timeout=timeout)
+
+        if response.status_code == 200:
+            return response.text
+        else:
+            print('Error en la solicitud. Código de respuesta HTTP:',
+                  response.status_code)
+            return None
+
+    except Exception as e:
+        print('Error en la solicitud:', str(e))
+        return None
+
+
+# Conectar a la red WiFi
+connectWifi(SSID, PASSWORD)
+
+# Se solicitan los datos e imprimen en consola
+data = fetchApi(URL_API)
+print(data)
