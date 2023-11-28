35feaeb1594e304a3d437332ceb17c0b06734da3
ScientistaMado <profesorsamuelcortes@gmail.com>
2023-11-27 22:07:17 -0300

    Se añadieron nuevos ejemplos

--- Templates/menu_oled_sh1106/example/menu_mix_sh1106.py
+++ Templates/menu_oled_sh1106/example/menu_mix_sh1106.py
@@ -5,7 +5,7 @@ import time
 
 
 def show_box():
-    oled.blit(main_menu.openIcon("box"), 80, 0)
+    oled.blit(main_menu.openIcon("box"), 80, 15)
     oled.show()
     main_menu.draw()
 
@@ -28,16 +28,59 @@ def show_icon_3():
     main_menu.draw()
 
 
+def show_main_menu():
+    oled.fill(0)
+    oled.text("Main menu:", 0, 0)
+    main_menu.draw()
+
+
+def show_menu_weather():
+    oled.fill(0)
+    oled.text("Weather menu:", 0, 0)
+    weather_menu.draw()
+
+
+def show_icon_w_sun():
+    oled.blit(weather_menu.openIcon("sun"), 80, 15)
+    oled.show()
+    weather_menu.draw()
+
+
+def show_icon_w_rain():
+    oled.blit(weather_menu.openIcon("rain"), 80, 15)
+    oled.show()
+    weather_menu.draw()
+
+
+def show_icon_w_cloudy():
+    oled.blit(weather_menu.openIcon("cloudy"), 80, 15)
+    oled.show()
+    weather_menu.draw()
+
+
 i2c = I2C(0, scl=Pin(17), sda=Pin(16))
 oled = sh1106.SH1106_I2C(128, 64, i2c, rotate=180)
 
+
 main_menu = MENU_OPTIONS(oled, y_init=10, width_menu=60, partial_update=True)
 
+main_menu.add_option("Box", show_box)
 main_menu.add_option("icono 1", show_icon_1)
 main_menu.add_option("icono 2", show_icon_2)
 main_menu.add_option("icono 3", show_icon_3)
+main_menu.add_option("Weather", show_menu_weather)
 
-menu_list = [main_menu]
+
+weather_menu = MENU_OPTIONS(
+    oled, y_init=10, width_menu=75, partial_update=True)
+
+weather_menu.add_option("Sun", show_icon_w_sun)
+weather_menu.add_option("Rain", show_icon_w_rain)
+weather_menu.add_option("cloudy", show_icon_w_cloudy)
+weather_menu.add_option("Main menu", show_main_menu)
+
+
+menu_list = [main_menu, weather_menu]
 
 menu = MENU_LIST(menu_list)
 
@@ -48,9 +91,7 @@ button_select = Pin(13, Pin.IN, Pin.PULL_DOWN)
 
 
 # Dibujar el menú
-oled.text("Main menu:", 0, 0)
-oled.show()
-main_menu.draw()
+show_main_menu()
 
 while True:
 
